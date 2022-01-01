787363869db5c76e26e3d1e27ab924cceadd8edf
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-01-01 16:03:19 +0000

    add tiny-invariant

--- app/person.ts
+++ app/person.ts
@@ -2,17 +2,24 @@ export type Person = {
   name: string;
 };
 
+let people = [
+  {
+    name: "Schüler1",
+  },
+  {
+    name: "Schüler2",
+  },
+  {
+    name: "Schüler3",
+  },
+];
+
 export function getPeople() {
-  const people: Person[] = [
-    {
-      name: "Schüler1",
-    },
-    {
-      name: "Schüler2",
-    },
-    {
-      name: "Schüler3",
-    },
-  ];
+  console.log("get People", people);
   return people;
+}
+
+export function createPerson(person: Person) {
+  people = [...people, person];
+  console.log("create Person", people);
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,43 @@
-import { useLoaderData } from "remix";
-import { Table, Container, Segment } from "semantic-ui-react";
-import { getPeople, Person } from "~/person";
+import {
+  useLoaderData,
+  Form,
+  redirect,
+  useActionData,
+  ActionFunction,
+} from "remix";
+import { Table, Container, Segment, Input } from "semantic-ui-react";
+import { createPerson, getPeople, Person } from "~/person";
+import invariant from "tiny-invariant";
+
+type NameError = {
+  name?: boolean;
+};
 
 export const loader = () => {
   return getPeople();
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const name = formData.get("name");
+
+  const errors: NameError = {};
+  if (!name || name.toString().trim().length === 0) errors.name = true;
+  if (Object.keys(errors).length) {
+    return errors;
+  }
+
+  invariant(typeof name === "string");
+
+  await createPerson({ name });
+
+  return redirect("/");
+};
+
 export default function Index() {
   const people = useLoaderData<Person[]>();
+  const errors = useActionData();
   return (
     <Container>
       <Segment>
@@ -19,11 +49,18 @@ export default function Index() {
           </Table.Header>
 
           <Table.Body>
-            {people.map(person => (
-              <Table.Row>
+            {people.map((person) => (
+              <Table.Row key={person.name}>
                 <Table.Cell>{person.name}</Table.Cell>
               </Table.Row>
             ))}
+            <Table.Row>
+              <Table.Cell>
+                <Form method="post">
+                  <Input placeholder="Neue Person..." name="name" />
+                </Form>
+              </Table.Cell>
+            </Table.Row>
           </Table.Body>
         </Table>
       </Segment>
