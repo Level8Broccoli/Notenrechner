6a8ae3f9ab16af3a8f8deb8060836f01581d55d2
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-01-01 13:36:43 +0000

    add list of names

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,27 @@
+import { Table, Container, Segment } from "semantic-ui-react";
+
+const names = ["Schüler1", "Schüler2"];
+
 export default function Index() {
   return (
-    <h1>test</h1>
+    <Container>
+      <Segment>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Namen</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {names.map(name => (
+              <Table.Row>
+                <Table.Cell>{name}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </Segment>
+    </Container>
   );
 }
